d7e91761976e0970b9ae5297bd65cea9dd46776f
arcticcloudfox <27johnson279@gmail.com>
2024-05-29 00:49:02 -0500

    test2

--- candidate-testing.js
+++ candidate-testing.js
@@ -13,7 +13,7 @@ let candidateAnswer = '';
 //TODO: Variables for Part 2
 let questions = ['Who was the first American woman in space? ', 'True or false: 5 kilometer == 5000 meters? ', '(5 + 3)/2 * 10 = ? ', "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ", 'What is the minimum crew size for the ISS? '];
 let correctAnswers = ['Sally Ride', 'true', '40', 'Trajectory', '3'];
-let candidateAnswers = [];
+let candidateAnswers = '';
 
 
 function askForName() {
@@ -27,6 +27,7 @@ function askQuestion() {
   for (let i = 0; i < questions.length; i++) {
     questions = input.question(question[i]);
     input = [];
+    candidateAnswers.toLowerCase();
     candidateAnswers.push(input);
   }
 }
@@ -39,14 +40,13 @@ function gradeQuiz(candidateAnswers) {
   for (let i = 0; i < questions.length; i++) {
     if (candidateAnswers[i] == correctAnswers[i]) {
       console.log(`Good job thats correct! Your answer: ${candidateAnswers[i]}. Correct answer: ${correctAnswers[i]}.`);
-      score = score + 1;
+      score++;
     } else {
       console.log(`Sorry that was wrong. Your answer: ${candidateAnswers[i]}. Correct answer: ${correctAnswers[i]}.`);
     }
-    let grade = (score/questions.length) * 100
+    let grade = (score/5) * 100;
     return grade;
   }
-
   //let grade = (score / questions.length) * 100; //TODO 3.2 use this variable to calculate the candidates score.
 
 
